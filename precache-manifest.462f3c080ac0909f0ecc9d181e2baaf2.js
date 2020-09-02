3c17b583c055f68d9143ffd61635169810daeb6b
jingyaopu <jingyaopu@gmail.com>
2020-09-02 17:42:33 -0400

    Updates

---
+++ precache-manifest.462f3c080ac0909f0ecc9d181e2baaf2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9496ab6c20de7b5ccd9733ff2eae2d2",
+    "url": "/react-flip-cards/index.html"
+  },
+  {
+    "revision": "ae6baa83de5566c61101",
+    "url": "/react-flip-cards/static/css/main.337e84b1.chunk.css"
+  },
+  {
+    "revision": "035781f573757a22d877",
+    "url": "/react-flip-cards/static/js/2.51c8687d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-flip-cards/static/js/2.51c8687d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae6baa83de5566c61101",
+    "url": "/react-flip-cards/static/js/main.c79fd2ee.chunk.js"
+  },
+  {
+    "revision": "64b34aaaa7879645615e",
+    "url": "/react-flip-cards/static/js/runtime-main.2b939c06.js"
+  },
+  {
+    "revision": "3ac88eddd1ac03bfc901de76424b5aba",
+    "url": "/react-flip-cards/static/media/5C.3ac88edd.png"
+  },
+  {
+    "revision": "5ab3d8627d0c4d17c86c73d90817900b",
+    "url": "/react-flip-cards/static/media/5S.5ab3d862.png"
+  },
+  {
+    "revision": "58d872f063752027e59e15af4d090123",
+    "url": "/react-flip-cards/static/media/7D.58d872f0.png"
+  },
+  {
+    "revision": "17ca9f77b395a41c6d45cbff350573bb",
+    "url": "/react-flip-cards/static/media/AC.17ca9f77.png"
+  },
+  {
+    "revision": "f37d608fb18056bc4f83737d633c5cf5",
+    "url": "/react-flip-cards/static/media/AD.f37d608f.png"
+  },
+  {
+    "revision": "a762540d55018bcf43ffba8b6343c9c9",
+    "url": "/react-flip-cards/static/media/AH.a762540d.png"
+  },
+  {
+    "revision": "8a890151c2b9cbd56a7242210babace6",
+    "url": "/react-flip-cards/static/media/blue_back.8a890151.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-flipCards/precache-manifest.1e9671fc7097c117cff1e251684f33a8.js"
+  "/react-flip-cards/precache-manifest.462f3c080ac0909f0ecc9d181e2baaf2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-flipCards/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-flip-cards/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
